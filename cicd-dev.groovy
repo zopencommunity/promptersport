84c3b3d3c2ca1b0510ee3a895aebd90d5e3bcf76
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:32 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/promptersport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/promptersport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/promptersport.git'), string(name: 'PORT_DESCRIPTION', value: 'A collection, growing, of shell scripts to guide/prompt the use of various shell commands' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/promptersport.git'), string(name: 'PORT_DESCRIPTION', value: 'A collection, growing, of shell scripts to guide/prompt the use of various shell commands' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,7 +6,7 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/promptersport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/promptersport.git']]])
     checkout([
       $class: 'GitSCM',
       branches: [[name: '*/main']],
@@ -15,6 +15,6 @@ node('linux')
       userRemoteConfigs: [[url: 'https://github.com/lbdyck/prompters.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/promptersport.git'), string(name: 'PORT_DESCRIPTION', value: 'A collection, growing, of shell scripts to guide/prompt the use of various shell commands' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/promptersport.git'), string(name: 'PORT_DESCRIPTION', value: 'A collection, growing, of shell scripts to guide/prompt the use of various shell commands' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
